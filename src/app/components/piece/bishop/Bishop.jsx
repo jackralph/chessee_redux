34d5fde371e5c948b410a92800ee4c850c6cc612
Jack R <jack.ralph@thedatashed.co.uk>
2023-05-04 08:15:45 +0100

    Remove console log in Bishop.jsx and rename boardSlice

--- src/app/components/piece/bishop/Bishop.jsx
+++ src/app/components/piece/bishop/Bishop.jsx
@@ -1,7 +1,6 @@
 export function Bishop(props) {
     const { color } = props;
 
-    console.log(color);
     return ( 
         <div style={{background: "transparent", border: "none"}}>
             <img src={`${process.env.PUBLIC_URL}/assets/piece/bishop_${color}.png`} alt="" style={{height: 75, width: 75}} />
--- src/features/board/boardSlice.js
+++ src/features/board/boardSlice.js
@@ -3,9 +3,9 @@ import { createSlice } from '@reduxjs/toolkit'
 import { setInitialBoardState } from '../../utils/board'
 
 export const boardSlice = createSlice({
-  name: 'counter',
-  initialState: { value: setInitialBoardState() },
-  reducers: {},
+    name: 'board',
+    initialState: { value: setInitialBoardState() },
+    reducers: {},
 })
 
 // Action creators are generated for each case reducer function
